045077bb9b886644f77f6b1be51c72c7b76ea4e2
Kevin Haas <29680216+xransum@users.noreply.github.com>
2025-01-16 22:16:45 -0500

    Create Youtube-Skip-Ahead.meta.js

---
+++ Youtube-Skip-Ahead.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Youtube Skip Ahead
+// @namespace    https://greasyfork.org/users/xransum
+// @version      1.0.0
+// @description  Simple auto-clicker for skipping sponsored segments with the "Skip Ahead" button for Premium users.
+// @author       xransum (https://github.com/xransum)
+// @match        https://www.youtube.com/watch*
+// @grant        none
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/xransum/Youtube-Skip-Ahead/refs/heads/main/Youtube-Skip-Ahead.user.js
+// @updateURL    https://raw.githubusercontent.com/xransum/Youtube-Skip-Ahead/refs/heads/main/Youtube-Skip-Ahead.meta.js
+// ==/UserScript==
